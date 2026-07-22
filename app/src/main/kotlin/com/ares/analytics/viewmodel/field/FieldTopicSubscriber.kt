140192af8f78889ebd57cd04ab7a8a8050cf0df7
David G Huss <34256796+thehomelessguy@users.noreply.github.com>
2026-07-22 14:30:52 -0400

    fix: subscribe to ARES/TruePose topic for ground truth field rendering to eliminate robot position teleporting

--- app/src/main/kotlin/com/ares/analytics/viewmodel/field/FieldTopicSubscriber.kt
+++ app/src/main/kotlin/com/ares/analytics/viewmodel/field/FieldTopicSubscriber.kt
@@ -45,15 +45,15 @@ class FieldTopicSubscriber(
                 val value = frame.value
                 
                 when (key) {
-                    "ARES/EstimatedPose/0" -> currentBuilder.trueX = value
-                    "ARES/EstimatedPose/1" -> currentBuilder.trueY = value
-                    "ARES/EstimatedPose/2" -> {
+                    "ARES/TruePose/0" -> currentBuilder.trueX = value
+                    "ARES/TruePose/1" -> currentBuilder.trueY = value
+                    "ARES/TruePose/2" -> {
                         currentBuilder.simHeading = value
                         currentBuilder.trueHeading = value
                     }
-                    "Drive/Pose_X" -> currentBuilder.ekfX = value
-                    "Drive/Pose_Y" -> currentBuilder.ekfY = value
-                    "Drive/Pose_Heading", "Drive/Drive_Heading" -> currentBuilder.ekfHeading = value
+                    "ARES/EstimatedPose/0", "Drive/Pose_X" -> currentBuilder.ekfX = value
+                    "ARES/EstimatedPose/1", "Drive/Pose_Y" -> currentBuilder.ekfY = value
+                    "ARES/EstimatedPose/2", "Drive/Pose_Heading", "Drive/Drive_Heading" -> currentBuilder.ekfHeading = value
 
                     "Drive/Odom_X", "pinpoint_x", "pinpoint/x" -> currentBuilder.odomX = value
                     "Drive/Odom_Y", "pinpoint_y", "pinpoint/y" -> currentBuilder.odomY = value
